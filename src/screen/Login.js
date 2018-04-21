c649eab7cbc7f4e27fd322326f2fa0e935901919
Fatimah Azzahra <fatimah.azzhr@gmail.com>
2018-04-21 18:36:36 +0700

    login, register, new game ui version 01

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Container,
+import {
   Header,
-  Content,
   Form,
   Item,
   Input,
   Label,
-  Button,
   Spinner,
   View,
   Text } from 'native-base';
 import { auth } from '../firebase/index';
-import { AsyncStorage } from 'react-native'
+import { AsyncStorage, StyleSheet, TouchableOpacity } from 'react-native'
 import { bindActionCreators } from 'redux'
+import LinearGradient from 'react-native-linear-gradient';
 
 import { loginwithEmail } from '../store/auth/auth-actions'
 
@@ -27,7 +26,7 @@ export class Login extends Component {
   }
 
   static navigationOptions = {
-    title: `Login`
+    title: `Carta`
   }
 
   onSignIn = () => {
@@ -51,55 +50,54 @@ export class Login extends Component {
 
   render() {
     return (
-      <Container>
-        <Text style={{ marginVertical:30, fontSize:36, textAlign:'center', fontFamily:'Didot' }} >
-          Let's Find What's Hidden
-        </Text>
-        {
-          this.props.authEmail.loading ?
-          <Content>
-            <Spinner color="blue" />
-          </Content>
-           : 
-          <Content style={{ paddingHorizontal:20 }}>
-            <Form style={{ paddingVertical:20 }}>
-              <Item fixedLabel last rounded style={{ marginVertical:10, backgroundColor:'white' }}>
-                <Label>Email</Label>
-                <Input
-                autoCapitalize='none'
-                name="email"
-                value={ this.state.email }
-                onChangeText={(email) => this.setState({email}) }
-                />
-              </Item>
-              <Item fixedLabel last rounded style={{ marginVertical:10, backgroundColor:'white' }}>
-                <Label>Password</Label>
-                <Input
-                secureTextEntry={true}
-                name="password"
-                type="password"
-                value={ this.state.password }
-                onChangeText={(password) => this.setState({password}) }/>
-              </Item>
-            </Form>
-              <Button
-              style={{ marginVertical:5, alignSelf:'center' }}
-              rounded
+      <LinearGradient colors={['#3E073E', '#210E3A']} style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center' }}>
+        <View>
+          <Text style={{ fontSize:33, textAlign:'center', fontFamily:'Didot', color: '#F1F1F4' }} >
+            Let's Find What's Hidden
+          </Text>
+          {
+            this.props.authEmail.loading ?
+            <View>
+              <Spinner color="white" />
+            </View>
+            : 
+            <View>
+              <Form style={{ paddingVertical:20 }}>
+                <Item fixedLabel last style={{ marginVertical:10, backgroundColor:'#B1AEC4', borderRadius: 10 }}>
+                  <Label style={{ fontFamily: 'futura', fontWeight: '500' }}>Email</Label>
+                  <Input
+                  autoCapitalize='none'
+                  name="email"
+                  value={ this.state.email }
+                  onChangeText={(email) => this.setState({email}) }
+                  />
+                </Item>
+                <Item fixedLabel last style={{ marginVertical:10, backgroundColor:'#B1AEC4', borderRadius: 10 }}>
+                  <Label style={{ fontFamily: 'futura', fontWeight: '500' }}>Password</Label>
+                  <Input
+                  secureTextEntry={true}
+                  name="password"
+                  type="password"
+                  value={ this.state.password }
+                  onChangeText={(password) => this.setState({password}) }/>
+                </Item>
+              </Form>
+              <TouchableOpacity
+              style={{ paddingVertical: 10, alignSelf:'center', width: '50%', borderRadius: 10, backgroundColor: '#F1F1F4', alignItems: 'center' }}
               success
               onPress={ () => this.onSignIn() }>
-                <Text>Login</Text>
-              </Button>
-              {/* <Text style={{ marginVertical:10 }}>Don't have an account yet?</Text> */}
-              <Button
-              style={{ marginVertical:5, alignSelf:'center' }}
-              rounded
-              success
-              onPress={ () => this.props.navigation.navigate('Register') }>
-                <Text>Register</Text>
-              </Button>
-          </Content>
-        }
-      </Container>
+                <Text style={{ fontFamily: 'futura', fontWeight: 'bold', color: '#3E073E', textAlign: 'center' }}>SIGN IN</Text>
+              </TouchableOpacity>
+              <View style={{ flexDirection: 'row', marginVertical: 15 }}>
+                <Text style={{ fontFamily: 'futura', color: '#F1F1F4', marginHorizontal: 5, fontWeight: '500' }}>Don't have an account yet?</Text>
+                <Text
+                  style={{ fontFamily: 'futura', color: '#F1F1F4', fontWeight: '500', textDecorationLine: 'underline' }}
+                  onPress={ () => this.props.navigation.navigate('Register') }>Register</Text>
+              </View>
+            </View>
+          }
+        </View>
+      </LinearGradient>
     );
   }
 }
@@ -111,5 +109,11 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => bindActionCreators ({
   loginwithEmail
 }, dispatch)
+
+var styles = StyleSheet.create({
+  text: {
+    color: '#D8D6E1'
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
